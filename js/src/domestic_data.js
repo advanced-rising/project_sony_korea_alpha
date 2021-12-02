4797807c9866942f00c5620e3eda8bee832cf08a
risingCore <vcjsm2283@gmail.com>
2021-12-02 23:01:42 +0900

    JS : innerHtml, inneText

---
+++ js/src/domestic_data.js
@@ -0,0 +1,257 @@
+var domesticData = [
+  {
+    "proImg": "../../img/prophotographer/domestic/list_choijihye.jpg",
+    "proTxt": "Sige",
+    "proFileImg": "../../img/prophotographer/domestic/pro_choijihye.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list_hokeunkim.jpg",
+    "proTxt": "Willeon Grardaye",
+    "proFileImg": "../../img/prophotographer/domestic/pro_hokeunkim.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list_janghaena.jpg",
+    "proTxt": "Gyles Elnnys",
+    "proFileImg": "../../img/prophotographer/domestic/pro_janghaena.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list_jangwon.jpg",
+    "proTxt": "Ichol",
+    "proFileImg": "../../img/prophotographer/domestic/pro_jangwon.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list_jinifoto.jpg",
+    "proTxt": "Glothher",
+    "proFileImg": "../../img/prophotographer/domestic/pro_jinifoto.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list_jisupan.jpg",
+    "proTxt": "Stephye",
+    "proFileImg": "../../img/prophotographer/domestic/pro_jisupan.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list_jk.jpg",
+    "proTxt": "Aerehrt",
+    "proFileImg": "../../img/prophotographer/domestic/pro_jk.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list_jungkisoo.jpg",
+    "proTxt": "Gauwis",
+    "proFileImg": "../../img/prophotographer/domestic/pro_jungkisoo.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list_junokim.jpg",
+    "proTxt": "Edic",
+    "proFileImg": "../../img/prophotographer/domestic/pro_junokim.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list_kangminbeom.jpg",
+    "proTxt": "Symes Byne",
+    "proFileImg": "../../img/prophotographer/domestic/pro_kangminbeom.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list_kimkyoungjin.jpg",
+    "proTxt": "Warder Masell",
+    "proFileImg": "../../img/prophotographer/domestic/pro_KimHyunsoo.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list_kimtaeeun.jpg",
+    "proTxt": "Aelfweald",
+    "proFileImg": "../../img/prophotographer/domestic/pro_kimkyoungjin.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list_kimyong.jpg",
+    "proTxt": "Aerert",
+    "proFileImg": "../../img/prophotographer/domestic/pro_kimtaeeun.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list_kimyoungcheol.jpg",
+    "proTxt": "Wine",
+    "proFileImg": "../../img/prophotographer/domestic/pro_kimyong.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list_KwonOChul.jpg",
+    "proTxt": "Here",
+    "proFileImg": "../../img/prophotographer/domestic/pro_kimyoungcheol.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list_leebongjoo.jpg",
+    "proTxt": "Narder Pary",
+    "proFileImg": "../../img/prophotographer/domestic/pro_KwonOChul.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list_leegyeongryeol.jpg",
+    "proTxt": "Cyne",
+    "proFileImg": "../../img/prophotographer/domestic/pro_leebongjoo.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list_leejaehyuk.jpg",
+    "proTxt": "Aerer",
+    "proFileImg": "../../img/prophotographer/domestic/pro_leegyeongryeol.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list_leejaein.jpg",
+    "proTxt": "Warder Rydaye",
+    "proFileImg": "../../img/prophotographer/domestic/pro_leejaehyuk.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list_leemyungjae.jpg",
+    "proTxt": "Gylex",
+    "proFileImg": "../../img/prophotographer/domestic/pro_leejaein.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list_mokenajung.jpg",
+    "proTxt": "Bertio",
+    "proFileImg": "../../img/prophotographer/domestic/pro_leemyungjae.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list_parkheungsoon.jpg",
+    "proTxt": "Eanrert",
+    "proFileImg": "../../img/prophotographer/domestic/pro_mokenajung2.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list_parkinsu.jpg",
+    "proTxt": "Beorhtio",
+    "proFileImg": "../../img/prophotographer/domestic/pro_parkheungsoon.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list_seokyonglee.jpg",
+    "proTxt": "Cuther",
+    "proFileImg": "../../img/prophotographer/domestic/pro_parkinsu.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list_somedaygraphy.jpg",
+    "proTxt": "Ilin Geddaye",
+    "proFileImg": "../../img/prophotographer/domestic/pro_seokyonglee.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list_songchului.jpg",
+    "proTxt": "Cyne",
+    "proFileImg": "../../img/prophotographer/domestic/pro_ShinJaeKuk.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list_sonjail.jpg",
+    "proTxt": "Grithels",
+    "proFileImg": "../../img/prophotographer/domestic/pro_somedaygraphy.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list_yumhoyoung.jpg",
+    "proTxt": "Reder Bertson",
+    "proFileImg": "../../img/prophotographer/domestic/pro_songchului.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list-choihs.jpg",
+    "proTxt": "Geoffry",
+    "proFileImg": "../../img/prophotographer/domestic/pro_sonjail.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list-JoowonKim.jpg",
+    "proTxt": "Unnehrt",
+    "proFileImg": "../../img/prophotographer/domestic/pro_yumhoyoung.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list-JungRaeCho.jpg",
+    "proTxt": "Wearda",
+    "proFileImg": "../../img/prophotographer/domestic/pro-choihs.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list-KimHyunsoo.jpg",
+    "proTxt": "Gauwis",
+    "proFileImg": "../../img/prophotographer/domestic/pro-JoowonKim.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list-Leejeongyeol.jpg",
+    "proTxt": "Gylex",
+    "proFileImg": "../../img/prophotographer/domestic/pro-JungRaeCho.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list-Leemh.jpg",
+    "proTxt": "Raffin Stere",
+    "proFileImg": "../../img/prophotographer/domestic/pro-Leejeongyeol.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list-RayKay.jpg",
+    "proTxt": "Warder Folcey",
+    "proFileImg": "../../img/prophotographer/domestic/pro-Leemh.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list-Seo.jpg",
+    "proTxt": "Gery",
+    "proFileImg": "../../img/prophotographer/domestic/pro-RayKay.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list-ShinJaeKuk.jpg",
+    "proTxt": "Lacio",
+    "proFileImg": "../../img/prophotographer/domestic/pro-Seo.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  },
+  {
+    "proImg": "../../img/prophotographer/domestic/list-StudioWonkyu.jpg",
+    "proTxt": "Wine",
+    "proFileImg": "../../img/prophotographer/overseas/pro-Nick.png",
+    "imgTxt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, error."
+  }
+]
+var proBox = document.getElementById('proBox');
+var makeUl = document.createElement('ul');
+var i = 0;
+var len = domesticData.length;
+var permissionCode = `<div class="pro_img"></div><div class="pro_txt"><span></span></div><div class="pro_profile"><div class="profile_img"><a href="#"><span></span></a></div><div class="img_txt"><a href="#"><p></p></a></div></div>`
+
+proBox.append(makeUl);
+for(; i<len;i++){
+  var makeLi = document.createElement('li');
+  makeLi.innerHTML = permissionCode;
+  makeUl.append(makeLi);
+}
+function proPhotoGrapherSetting(n){
+  var index = n;
+  var li = makeUl.querySelectorAll('li');
+  var proImg = li[index].querySelector('.pro_img');
+  var proTxt = li[index].querySelector('.pro_txt span');
+  var proFileImg = li[index].querySelector('.profile_img');
+  var imgTxt = li[index].querySelector('.img_txt p');
+  proImg.style.backgroundImage = 'url('+domesticData[index].proImg+')';
+  proTxt.innerText = domesticData[index].proTxt;
+  proFileImg.style.backgroundImage = 'url('+domesticData[index].proFileImg+')';
+  imgTxt.innerText = domesticData[index].imgTxt;
+}
+domesticData.forEach(function(d,i){
+  proPhotoGrapherSetting(i);
+});
