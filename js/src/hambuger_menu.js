bbc436562b2d03c154b05722b3ebe71cf634a4c7
risingcore <vcjsm2283@gmail.com>
2021-12-08 18:36:19 +0900

    style [ scss ] : supports page responsive web

--- js/src/hambuger_menu.js
+++ js/src/hambuger_menu.js
@@ -1,24 +1,24 @@
-var headBox = document.querySelector('#headBox');
-var navigation = headBox.querySelector('#navigation');
-var gnbBtn = navigation.querySelector('.gnb_btn');
-var gnbClose = navigation.querySelector('.gnb_close');
+let headBox = document.querySelector('#headBox');
+let navigation = headBox.querySelector('#navigation');
+let gnbBtn = navigation.querySelector('.gnb_btn');
+let gnbClose = navigation.querySelector('.gnb_close');
 
-var naviArea = navigation.querySelector('.navi_area');
-var gnbUl = naviArea.querySelector('ul');
-var gnbLi = gnbUl.children;
-var gnbLiArr = Array.prototype.slice.call(gnbLi);
-var subArea = naviArea.querySelectorAll('.sub_area');
+let naviArea = navigation.querySelector('.navi_area');
+let gnbUl = naviArea.querySelector('ul');
+let gnbLi = gnbUl.children;
+let gnbLiArr = Array.prototype.slice.call(gnbLi);
+let subArea = naviArea.querySelectorAll('.sub_area');
 
 
-var VIEW_ON = 'on';
-var ACTIVE_ON = 'active';
+let VIEW_ON = 'on';
+let ACTIVE_ON = 'active';
 
 
 // navigation gnbBtn 클릭시 메뉴 오픈
 gnbBtn.addEventListener('click', function(e){
   e.preventDefault();
-  var VIEW_ON = 'on';
-  var isActive = naviArea.classList.contains(VIEW_ON);
+  let VIEW_ON = 'on';
+  let isActive = naviArea.classList.contains(VIEW_ON);
   if(!isActive){
     naviArea.classList.add(VIEW_ON);
     gnbBtn.classList.add(ACTIVE_ON);
@@ -30,9 +30,9 @@ gnbBtn.addEventListener('click', function(e){
 });
 
 // 서브메뉴 목록이 없다면 a를 클릭 , 있다면 서브메뉴를 나타나게 하라 .
-var i = 0;
-var j = 0;
-var len = gnbLiArr.length;
+let i = 0;
+let j = 0;
+let len = gnbLiArr.length;
 for(i = 0; i < subArea.length ; i++){
   (function(k){
     subArea[k].previousSibling.previousSibling.addEventListener('click', function(e){
@@ -45,17 +45,20 @@ for(i = 0; i < subArea.length ; i++){
   })(i);
 };
 // 아 .. 왜 ??? 안됨 ?? remove 안됨 ;';;; 아 됨 ㅎㅎ ㅋㅋ
+// 변수 선언 안해줌 ㅋㅋ
+// subArea의 parentNode 를 잡고, 그 부모요소의 바로 자식인 a tag를 childNode로 잡아서
+// childeNode === node_element ... 
 
-var viewBox = document.querySelector('#viewBox');
-var viewArea = viewBox.querySelector('.view_area');
-var viewUl = viewArea.querySelector('ul');
-var viewLi = viewUl.querySelectorAll('.viewBox_img');
+let viewBox = document.querySelector('#viewBox');
+let viewArea = viewBox.querySelector('.view_area');
+let viewUl = viewArea.querySelector('ul');
+let viewLi = viewUl.querySelectorAll('.viewBox_img');
 console.log(viewLi);
 
-var viewIndicator = viewBox.querySelector('.view_indicator');
-var viewIndicatorUl = viewIndicator.querySelector('ul');
-var viewIndicatorLi = viewIndicatorUl.querySelectorAll('li');
+let viewIndicator = viewBox.querySelector('.view_indicator');
+let viewIndicatorUl = viewIndicator.querySelector('ul');
+let viewIndicatorLi = viewIndicatorUl.querySelectorAll('li');
 console.log(viewIndicatorLi);
 
-var slideLength = viewLi.length;
+let slideLength = viewLi.length;
 // 광고슬라이드 영역할 준비
