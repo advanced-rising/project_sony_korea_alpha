c37d2c12b02bcd25c9e3acf1f74d64ce8c60fa7d
risingcore <vcjsm2283@gmail.com>
2021-12-29 10:18:01 +0900

    fix [ JSON ] : json 값 오류 해결 완료

--- dist/src/esBox.js
+++ dist/src/esBox.js
@@ -1,13 +1,20 @@
-// const esBoxPath = "../data/esBoxData.json";
-// fetch(esBoxPath)
-
-// // fetch(esBoxPath).then((response)=> { return response.json()}).then((response)=>{
-// //   const dataArr = [...response]
-// //   console.log(dataArr)
-// //   const esBoxCode =`<a href="#">
-// //     <div class="event_img"><span class="blind"></span></div></a>
-// //   <div class="event_txt"><span class="put_in_btn"></span><p></p></div>`
+const esBoxDataCode = "../data/esBoxData.json";
+fetch(esBoxDataCode)
+.then((response)=> response.json())
+.then((response)=>{
+  const dataArr = [...response];
+  const esBoxCode = `<a href="#">
+    <div class="event_img"><span class="blind"></span></div></a>
+    <div class="event_txt"><span class="put_in_btn"></span><p></p></div>`
+  console.log(response)
+})
+// fetch(esBoxPath).then((response)=> { return response.json()}).then((response)=>{
+//   const dataArr = [...response]
+//   console.log(dataArr)
+//   const esBoxCode =`<a href="#">
+//     <div class="event_img"><span class="blind"></span></div></a>
+//   <div class="event_txt"><span class="put_in_btn"></span><p></p></div>`
   
-// //   const makeLi = document.createElement('li');
-// // })
+//   const makeLi = document.createElement('li');
+// })
 
